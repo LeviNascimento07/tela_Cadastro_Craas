23fd8ed5657296247c1cebe056ac2d203118f709
ZNitr0 <levidonascimento2410@gmail.com>
2025-10-17 14:21:24 -0300

    ajustando css

--- tela-cadastro/src/App.jsx
+++ tela-cadastro/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AuthPage from "./pages/AuthPages"; 
 import "./styles/App.css";
+import logoPrefeitura from "../public/prefeitura-de-fortaleza-seeklogo.png";
 
 export default function App() {
   return (
     <div className="Principal">
-      <div>
+      <div className="Header">
+          <img src={logoPrefeitura} alt="Prefeitura de Fortaleza" className="logo-prefeitura" />
         <h1 className="Text-Primary">Bem-vindo ao Sistema CRAS</h1>
       </div>
 
